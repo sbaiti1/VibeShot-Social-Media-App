0e3eb05c8b66aa76cfbbdcf0573cada1ffabf457
salma sbaiti <salmasbaiti1@gmail.com>
2023-02-08 22:30:16 +0100

    add post ui done

--- resources/js/components/addPost.jsx
+++ resources/js/components/addPost.jsx
@@ -3,7 +3,10 @@ import { Avatar } from '@mui/material';
 import GifIcon from '@mui/icons-material/Gif';
 import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
 import MoodIcon from '@mui/icons-material/Mood';
-import { pink } from '@mui/material/colors';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import GifBoxIcon from '@mui/icons-material/GifBox';
+import '../../css/addPost.css'
+
 export default function AddPost(){
 
     return(
@@ -17,10 +20,10 @@ export default function AddPost(){
                 <form action="">
                     <input className="textField" type="text" placeholder="share your vibe !" />
                     <div className="icons">
-                        <InsertPhotoIcon fontSize="small"/>
-                        <GifIcon  fontSize="small"/>
-                        <MoodIcon fontSize="small"/>
-                        <MoodIcon fontSize="small" sx={{ color: pink[500] }} />
+                        <InsertPhotoIcon sx={{fill : '#38B6FF'}} fontSize="small"/>
+                        <LocationOnIcon sx={{fill : '#38B6FF'}} fontSize="small"/>
+                        <GifBoxIcon sx={{fill : '#38B6FF'}} fontSize="small"/>
+                        <MoodIcon sx={{fill : '#38B6FF'}} fontSize="small" />
                     </div>
                 </form>
             </div>

f24c56c331d749825ba1b89b3d0ba5e129b1f34d
salma sbaiti <salmasbaiti1@gmail.com>
2023-02-10 23:35:23 +0100

    trends added

---
+++ resources/js/components/trending.jsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
+import {Typography} from '@mui/material';
+import trendIcon from '../../../public/icons/trending.png'
+
+const Btn = styled(Button)(({ theme }) => ({
+    borderRadius : 50 , 
+    background : 'rgba(71, 85, 105, 0.62)' , 
+    width: '79px' , 
+    height:' 25px' , 
+    color: '#D2DBE5' , 
+    textTransform : 'lowercase'
+
+
+ 
+}));
+const Trend = styled(Box)(()=>({
+    display : 'flex' , 
+    alignItems : 'center' , 
+    justifyContent : 'space-between'
+}))
+
+const trendingData = [{tag : 'gym_life' , count : 90} ,
+{tag : 'healthy_meals' , count : 50} , 
+{tag : 'fashion' , count : 70}
+]
+
+export default function Trending() {
+  return (
+    <Box sx={{ width: '100%' ,border:' 2px solid #64748B' , 
+    borderRadius: '10px' , padding : '1rem ' }}>
+        <Typography mb={2} variant='h6' >
+                <img src={trendIcon} alt="" />
+                 Today's Trending
+        </Typography>
+      <Stack spacing={2}>
+            {trendingData.map((x , i)=>(
+                <Trend key={i}>
+                    <Typography sx={{fontWeight : 600}} variant='paragraph' > {x.tag} </Typography>
+                    <Btn> {x.count} posts </Btn>
+                </Trend>
+            ))}
+      </Stack>
+    </Box>
+  );
+}

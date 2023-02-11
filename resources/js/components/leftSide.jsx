92cf373fce0cf2ef645841846eed04c8bf61fc56
salma sbaiti <salmasbaiti1@gmail.com>
2023-02-11 20:45:24 +0100

    small update

--- resources/js/components/leftSide.jsx
+++ resources/js/components/leftSide.jsx
@@ -3,12 +3,16 @@ import '../../css/leftSection.css'
 import Search from './search';
 import Trending from './trending';
 import { Stack } from '@mui/system';
+import Suggestions from './suggestions';
+import Footer from './footer';
 
 export default function LeftSection (){
     return(
-        <Stack spacing={6} className='leftSection' >
+        <Stack spacing={3} className='leftSection' >
             <Search />
             <Trending />
+            <Suggestions />
+            <Footer />
         </Stack>
     )
 }

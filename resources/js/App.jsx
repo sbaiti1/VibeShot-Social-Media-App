80388b94e97f52f0a0ecb13aebef96b903391b1e
salma sbaiti <salmasbaiti1@gmail.com>
2023-03-21 21:17:20 +0000

    register route and component added

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/home';
 import Layout from './components/layout';
 import '../css/app.css'
 import SideNav from './components/sideNav';
+import Register from './components/Register/register';
 
 export default function App(){
     return(
@@ -14,6 +15,8 @@ export default function App(){
                 <Route path='/notifications' element={<Layout title='notifications'  />} />
                 <Route path='/bookmarks' element={<Layout title='bookmarks' />} />
                 <Route path='/settings' element={<Layout title='settings' />} />
+                <Route path='/register' element={<Register />} />
+
             </Routes>
         </BrowserRouter>
     )
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/register', function () {
+    return view('register');
+});
